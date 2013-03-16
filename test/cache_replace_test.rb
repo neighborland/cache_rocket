47b7555dd391e6e733775063889b8bb5a0ed21bb
Tee Parham <tee@neighborland.com>
2013-03-15 22:08:49 -0600

    tests!

---
+++ test/cache_replace_test.rb
@@ -0,0 +1,57 @@
+require 'test_helper'
+
+class CacheReplaceTest < Test::Unit::TestCase
+  class FakeRenderer
+    include CacheReplace
+
+    # mock ActionView::Helpers::OutputSafetyHelper
+    def raw(value)
+      value
+    end
+  end
+
+  setup do
+    @renderer = FakeRenderer.new
+  end
+
+  context "#cache_replace_key" do
+    should "return key with prefix" do
+      assert_equal CacheReplace::CACHE_FRAGMENT_KEY + "some/thing", @renderer.cache_replace_key("some/thing")
+    end
+  end
+
+  context "#render_cached" do
+    setup do
+      @renderer.stubs(:render).with("container", {}).returns "Fanny pack #{@renderer.cache_replace_key('inner')} viral mustache."
+      @renderer.stubs(:render).with("inner", {}).returns "quinoa hoodie"
+    end
+
+    should "render with single partial" do
+      assert_equal "Fanny pack quinoa hoodie viral mustache.", @renderer.render_cached("container", replace: "inner")
+    end
+
+    should "pass options to inner render" do
+      @renderer.expects(:render).with("container", variable: "x").returns ""
+      @renderer.expects(:render).with("inner", variable: "x").returns ""
+      @renderer.render_cached("container", variable: "x", replace: "inner")
+    end
+
+    should "render with array of partials" do
+      @renderer.stubs(:render).with("container", {}).
+        returns "#{@renderer.cache_replace_key('inner')} #{@renderer.cache_replace_key('other')} viral mustache."
+      @renderer.stubs(:render).with("other", {}).returns "high life"
+      assert_equal "quinoa hoodie high life viral mustache.", @renderer.render_cached("container", replace: ["inner", "other"])
+    end
+
+    should "render with map of keys" do
+      assert_equal "Fanny pack keytar viral mustache.", @renderer.render_cached("container", replace: {inner: "keytar"})
+    end
+
+    should "render with hash block" do
+      output = @renderer.render_cached("container") do
+        {inner: "keytar"}
+      end
+      assert_equal "Fanny pack keytar viral mustache.", output
+    end
+  end
+end
